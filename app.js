f2edd19affe81f0dbc5f80eb7e762b190dbb84d8
Robin Hawkes <rob.hawkes@gmail.com>
2015-01-19 14:49:06 +0000

    Loads of improvements to the browse page UI

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use(passport.session());
 app.use(multer({dest: "./tmp/"}));
 
 // Pagination defaults
-app.use(paginate.middleware(15, 50));
+app.use(paginate.middleware(10, 50));
 
 // Serve static files from directory
 app.use("/", express.static(path.join(__dirname, "public")));

2008a3ed1a96e91cd420e954835c2f1257b987e9
Robin Hawkes <rob@rawkes.com>
2015-04-02 10:10:15 +0100

    Adding tests

---
+++ test/test-api.js
@@ -0,0 +1,4 @@
+// TODO: Add tests for adding a new building
+// TODO: Add tests for modifying an existing building
+// TODO: Add tests for various building download options
+// TODO: Add tests for area-based options (eg. /api/buildings/bbox/:west,:south,:east,:north/:kml)
--- test/test-site.js
+++ test/test-site.js
@@ -15,9 +15,9 @@ describe("/", function () {
   });
 });
 
-describe("/browse", function () {
+describe("/contributing", function () {
   it("should be accessible", function(done) {
-    api.get("/")
+    api.get("/contributing")
     .expect(200)
     .end(function(err, res) {
       if (err) return done(err);
@@ -26,9 +26,64 @@ describe("/browse", function () {
   });
 });
 
-describe("/browse_all", function () {
+describe("/terms", function () {
   it("should be accessible", function(done) {
-    api.get("/")
+    api.get("/terms")
+    .expect(200)
+    .end(function(err, res) {
+      if (err) return done(err);
+      done();
+    });
+  });
+});
+
+describe("/browse", function () {
+  it("should be accessible", function(done) {
+    api.get("/browse")
+    .expect(200)
+    .end(function(err, res) {
+      if (err) return done(err);
+      done();
+    });
+  });
+});
+
+describe("/browse/all", function () {
+  it("should be accessible", function(done) {
+    api.get("/browse/all")
+    .expect(200)
+    .end(function(err, res) {
+      if (err) return done(err);
+      done();
+    });
+  });
+});
+
+describe("/search", function () {
+  it("should be accessible", function(done) {
+    api.get("/search")
+    .expect(200)
+    .end(function(err, res) {
+      if (err) return done(err);
+      done();
+    });
+  });
+});
+
+describe("/search/testing", function () {
+  it("should be accessible", function(done) {
+    api.get("/search/testing")
+    .expect(200)
+    .end(function(err, res) {
+      if (err) return done(err);
+      done();
+    });
+  });
+});
+
+describe("/search/near/-0.01924,51.50358,1000", function () {
+  it("should be accessible", function(done) {
+    api.get("/search/near/-0.01924,51.50358,1000")
     .expect(200)
     .end(function(err, res) {
       if (err) return done(err);
@@ -39,7 +94,7 @@ describe("/browse_all", function () {
 
 describe("/signup", function () {
   it("should be accessible", function(done) {
-    api.get("/")
+    api.get("/signup")
     .expect(200)
     .end(function(err, res) {
       if (err) return done(err);
@@ -50,8 +105,41 @@ describe("/signup", function () {
 
 describe("/login", function () {
   it("should be accessible", function(done) {
-    api.get("/")
+    api.get("/login")
     .expect(200)
+    .end(function(err, res) {
+      if (err) return done(err);
+      done();
+    });
+  });
+});
+
+describe("/logout", function () {
+  it("should redirect to index", function(done) {
+    api.get("/logout")
+    .expect(302)
+    .end(function(err, res) {
+      if (err) return done(err);
+      done();
+    });
+  });
+});
+
+describe("/forgot", function () {
+  it("should be accessible", function(done) {
+    api.get("/forgot")
+    .expect(200)
+    .end(function(err, res) {
+      if (err) return done(err);
+      done();
+    });
+  });
+});
+
+describe("/add", function () {
+  it("should redirect to login when not logged in", function(done) {
+    api.get("/add")
+    .expect(302)
     .end(function(err, res) {
       if (err) return done(err);
       done();

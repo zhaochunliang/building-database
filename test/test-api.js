b3debc237d8a9ffa0247014daef88cd26600705e
Robin Hawkes <rob@rawkes.com>
2015-04-10 09:45:14 +0100

    Removed old API tests for now and added NPM test command

--- test/test-api.js
+++
@@ -1,4 +0,0 @@
-// TODO: Add tests for adding a new building
-// TODO: Add tests for modifying an existing building
-// TODO: Add tests for various building download options
-// TODO: Add tests for area-based options (eg. /api/buildings/bbox/:west,:south,:east,:north/:kml)
